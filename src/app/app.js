2120077cccf440e798028b1a0d6fbc39dcd68b31
Doug Beasley <beasley.doug@gmail.com>
2016-06-10 16:10:26 -0700

    adding routes and focus function

--- src/app/app.js
+++ src/app/app.js
@@ -1,20 +1,45 @@
 (function() {
-'use strict'
+  'use strict'
 
 
 
-  var app = angular.module('app', ['ngMaterial']);
+  var app = angular.module('app', ['ngMaterial','ngRoute']);
 
 
+  app.config(function($routeProvider, $locationProvider) {
+  $routeProvider
+  .when('/one', {
+    templateUrl: 'one.html',
+    controller: 'SimpleController'
+  })
+  .when('/two', {
+    templateUrl: 'two.html',
+    controller: 'DataController'
+  });
 
-app.controller('AppController', ['$scope', '$mdSidenav', function($scope, $mdSidenav) {
+  // configure html5 to get links working on jsfiddle
+  $locationProvider.html5Mode(true);
+});
 
-  $scope.toggleNav = function() {
-    $mdSidenav('left').toggle();
-  };
+  app.controller('AppController', ['$scope', '$mdSidenav', function($scope, $mdSidenav) {
 
+    $scope.toggleNav = function() {
+      $mdSidenav('left').toggle();
+    };
 
-}]);
+    $scope.setFocus = function() {
+      $mdSidenav('left').close();
+    }
+
+  }]);
+
+  app.controller('SimpleController', function() {
+
+  });
+
+  app.controller('DataController', function() {
+
+  });
 
 
 })();
